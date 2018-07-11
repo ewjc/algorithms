8430f6cb84dee6d7612b9b8fcadf3c37d90c9588
Eric Wong <ericswong7@gmail.com>
2018-07-11 15:45:53 -0700

    updated algorithm practice

--- AlgorithmProblems.playground/Contents.swift
+++ AlgorithmProblems.playground/Contents.swift
@@ -3,3 +3,27 @@
 import UIKit
 
 var str = "Hello, playground"
+
+
+// Problem 1: Frequency of characters
+
+var characterArray = ["b", "c", "a", "z", "b", "a", "v"]
+
+func findFrequencyOfCharacters(array: [String]) -> [String : Int] {
+    
+    guard array.count > 0 else { return }
+    
+    var frequencyOfCharacterDict = [String : Int]()
+
+    for character in characterArray {
+        if characterArray.contains(character) {
+            var totalValuesInCharacter = frequencyOfCharacterDict[character] + 1
+            frequencyOfCharacterDict.updateValue(totalValuesInCharacter, forKey: character)
+            
+        } else if !characterArray.contains(character) {
+            frequencyOfCharacterDict.updateValue(1, forKey: character)
+        }
+    }
+    
+    return frequencyOfCharacterDict
+}
