a545998ca66a7798339e3425c15e89fd14314d29
Eric Wong <ericswong7@gmail.com>
2018-07-11 16:25:46 -0700

    fixed some problem 1 issue

--- AlgorithmProblems.playground/Contents.swift
+++ AlgorithmProblems.playground/Contents.swift
@@ -2,38 +2,37 @@
 
 import UIKit
 
-var str = "Hello, playground"
-
-
 // Problem 1: Frequency of characters
-
 var characterArray = ["b", "c", "a", "z", "b", "a", "v"]
 
 func findFrequencyOfCharacters(array: [String]) -> [String : Int] {
     
     guard array.count > 0 else { return [String : Int]() }
     
-    var frequencyOfCharacterDict = [String : Int]()
+    var frequencyAndCharacterDict = [String : Int]()
 
-    for character in characterArray {
-        if characterArray.contains(character) {
-            var totalValues: Int = 0
-            frequencyOfCharacterDict.updateValue(totalValues, forKey: character)
-        } else if !characterArray.contains(character) {
-            frequencyOfCharacterDict.updateValue(1, forKey: character)
+    for character in array {
+        if frequencyAndCharacterDict[character] == nil {
+            frequencyAndCharacterDict.updateValue(1, forKey: character)
+        } else {
+            let frequencyOfCharacter = frequencyAndCharacterDict[character] as! Int
+            var totalFrequencyAmount = frequencyOfCharacter + 1
+            frequencyAndCharacterDict.updateValue(totalFrequencyAmount, forKey: character)
         }
     }
     
-    return frequencyOfCharacterDict
+    return frequencyAndCharacterDict
+    print("this is the frequency and characters: \(frequencyAndCharacterDict)")
 }
 
+findFrequencyOfCharacters(array: characterArray)
 
 // Problem 2: Shift indices in array
 
 var randomArrayOfNumbers = [1, 3, 2, 7, 9, 10, 14, 2, 3, 1]
 
 func shiftIndicesInArray(array: [Int], shiftPositionAmount: Int) -> [Int] {
-    guard array.count > 0 else { return }
+    guard array.count > 0 else { return array }
     
     return [0]
     
